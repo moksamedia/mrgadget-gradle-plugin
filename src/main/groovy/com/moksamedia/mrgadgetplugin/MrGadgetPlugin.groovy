e7abc7899d7fd1ccaf0c3e3fe2c9e626a4320cef
moksamedia <andrewcarterhughes@gmail.com>
2012-09-06 22:07:42 -0500

    initial commit

---
+++ src/main/groovy/com/moksamedia/mrgadgetplugin/MrGadgetPlugin.groovy
@@ -0,0 +1,91 @@
+package com.moksamedia.mrgadgetplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import com.moksamedia.mrgadget.MrGadget
+
+class MrGadgetPlugin implements Plugin<Project> {
+
+	MrGadget mrg
+
+	void apply(Project project) {
+
+		project.extensions.mrgadget =  new MrGadgetPluginExtension()
+
+		project.metaClass.initMrGadget = { def params = [:] ->
+			def toUse = project.extensions.mrgadget.properties
+			toUse.putAll(params)
+			toUse.remove('metaClass') // not strictly necessary, but why not?
+			toUse.remove('class')
+			mrg?.closeSession()
+			mrg = new MrGadget(toUse)
+		}
+
+		project.metaClass."$project.extensions.mrgadget.execRemoteMethodName" = { def params = [:] ->
+			if (mrg==null) initMrGadget(params)
+			else mrg.setParams(params)
+			String command = params.command
+			assert command != null
+			mrg.execRemote(command)
+		}
+
+		project.metaClass."$project.extensions.mrgadget.execRemoteSudoMethodName" = { def params = [:] ->
+			if (mrg==null) initMrGadget(params)
+			else mrg.setParams(params)
+			String command = params.command
+			assert command != null
+			mrg.execRemote(command)
+		}
+
+		project.metaClass."$project.extensions.mrgadget.copyToRemoteSCPMethodName" = { def params = [:] ->
+			if (mrg==null) initMrGadget(params)
+			else mrg.setParams(params)
+			mrg.copyToRemoteSCP([localFile:params.localFile, remoteFile:params.remoteFile])
+		}
+
+		project.metaClass."$project.extensions.mrgadget.copyToRemoteSFTPMethodName" = { def params = [:] ->
+			if (mrg==null) initMrGadget(params)
+			else mrg.setParams(params)
+			mrg.copyToRemoteSFTP([localFile:params.localFile, remoteFile:params.remoteFile])
+		}
+
+		project.metaClass.closeMrGadgetSession = {
+			mrg?.closeSession()
+		}
+
+		project.metaClass.clearMrGadgetPasswords = {
+			if (mrg==null) initMrGadget(clearAllPasswords:true)
+			else mrg.clearAllPasswords()
+		}
+		
+		project.metaClass.getMrGadget = {
+			mrg
+		}
+
+	}
+
+}
+	
+class MrGadgetPluginExtension {
+
+	def String user
+	def String host
+
+	def String prefEncryptionKey = null
+	def String password = null
+	def String sudoPassword = null
+	
+	def int logProgressGranularity = 10
+
+	def boolean leaveSessionOpen = false
+	def boolean sudoPassDifferent = false
+	def boolean promptToSavePass = true
+	def boolean strictHostKeyChecking = true
+	def boolean showProgressDialog = true
+	def boolean preserveTimestamp = false
+
+	def String execRemoteMethodName = "execRemote"
+	def String execRemoteSudoMethodName = "execRemoteSudo"
+	def String copyToRemoteSCPMethodName = "copyToRemoteSCP"
+	def String copyToRemoteSFTPMethodName = "copyToRemoteSFTP"
+}
